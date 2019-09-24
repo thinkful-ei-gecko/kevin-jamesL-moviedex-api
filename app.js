ddffdf7ac4c030949ce59a1b5eaf0f807a1fa315
James Lee <jameswlee109@gmail.com>
2019-09-24 14:49:17 -0500

    assignment complete

--- app.js
+++ app.js
@@ -2,38 +2,36 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const helmet = require('helmet')
 const moviesData = require('./movies-data-small.json');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
+ap.use(helmet());
 
-// app.use(function validateBearerToken(req, res, next) {
-//   const apiToken = process.env.API_TOKEN;
-//   const authToken = req.get('Authorization');
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
 
-//   if (!authToken || authToken.split(' ')[1] !== apiToken) {
-//     return res.status(401).json({ error: 'Unauthorized request' });
-//   }
-//   // move to the next middleware
-//   next();
-// });
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
+});
 
 app.get('/movie', (req, res) => {
   const { genre, country, avg_vote } = req.query;
   let response = moviesData;
 
-  if ('genre' in req.query) {
-    if (genre) {
-      response = response.filter((r) =>
-        r.genre.toLowerCase().includes(genre.toLowerCase())
-      );
-    } else {
-      return res.status(400).json({ error: 'Genre cannot be blank' });
-    }
+  if (genre) {
+    response = response.filter((r) =>
+      r.genre.toLowerCase().includes(genre.toLowerCase())
+    );
   }
-
+  
   if (country) {
     response = response.filter((r) =>
       r.country.toLowerCase().includes(country.toLowerCase())
@@ -41,7 +39,7 @@ app.get('/movie', (req, res) => {
   }
 
   if (avg_vote) {
-    response = response.filter((r) => r.avg_vote >= avg_vote);
+    response = response.filter((r) => Number(r.avg_vote) >= Number(avg_vote));
   }
 
   res.json(response);
