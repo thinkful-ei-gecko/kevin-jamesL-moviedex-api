01a275f1f5ba6677e505c8da572b0fafc596cd34
Kevin Wei <chenghaowei@gmail.com>
2019-09-24 14:35:17 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const moviesData = require('./movies-data-small.json');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+// app.use(function validateBearerToken(req, res, next) {
+//   const apiToken = process.env.API_TOKEN;
+//   const authToken = req.get('Authorization');
+
+//   if (!authToken || authToken.split(' ')[1] !== apiToken) {
+//     return res.status(401).json({ error: 'Unauthorized request' });
+//   }
+//   // move to the next middleware
+//   next();
+// });
+
+app.get('/movie', (req, res) => {
+  const { genre, country, avg_vote } = req.query;
+
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
