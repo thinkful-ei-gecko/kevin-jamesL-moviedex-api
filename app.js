6c2e11041dfc89c4a0435b43ea4eb71e6412f7ff
James Lee <jameswlee109@gmail.com>
2019-09-24 14:53:38 -0500

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ ap.use(helmet());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization');
+  const authToken = req.get('authorization');
 
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
